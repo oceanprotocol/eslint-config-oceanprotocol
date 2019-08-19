faf1e861c887d5915b323b7072c77d0f5ef9a4ed
Matthias Kretschmann <m@kretschmann.io>
2019-08-19 11:29:42 +0200

    add prettier

--- react.js
+++ react.js
@@ -1,20 +1,20 @@
 const base = require('./index.js')
 
 const reactRules = {
-    'jsx-quotes': ['error', 'prefer-double'],
-    'react/jsx-indent': [2, 4],
-    'react/jsx-indent-props': [2, 4]
+  'jsx-quotes': ['error', 'prefer-double'],
+  'react/jsx-indent': [2, 4],
+  'react/jsx-indent-props': [2, 4]
 }
 
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:security/recommended',
-        'standard',
-        'standard-react'
-    ],
-    'plugins': ['security'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:security/recommended',
+    'standard',
+    'standard-react'
+  ],
+  plugins: ['security'],
 
-    rules: Object.assign({}, base.rules, reactRules)
+  rules: Object.assign({}, base.rules, reactRules)
 }
